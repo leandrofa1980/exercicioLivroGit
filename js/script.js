35efd02ebff9a9a977a7c7e8f7034b04a426e0a3
Leandro <leandrofa1980@gmail.com>
2020-10-23 15:16:52 -0300

    Script de troca de banner

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ window.addEventListener('load', start);
 console.log('Olá, mundo');
 
 function start() {}
+
+let banners = ['Os melhores do Brasil!', 'Qualidade e preço baixo!'];
+let bannerAtual = 0;
+
+function trocarBanner() {
+  bannerAtual = (bannerAtual + 1) % 2;
+  document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocarBanner, 2000);
